48e3a920ac903ab40d474005d5e69499ef5e7b79
SHUBHAM-ACHARYA <shubhamacharya113@gmail.com>
2022-01-28 03:11:25 +0530

    first commit

---
+++ apps/todos/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Todo {
+  title: string;
+}
+
+@Component({
+  selector: 'myorg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
+}
